1d7ebd73ca0a6cf12ce7eaf301c0d65be368f06d
codingconcepts <bassrob@gmail.com>
2021-10-24 19:30:52 +0100

    feat: Unicode generation

--- pa55.go
+++ pa55.go
@@ -9,14 +9,25 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math"
+	"math/big"
 	"strings"
 
 	"github.com/atotto/clipboard"
 )
 
-func main() {
-	ascii := " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
+var (
+	ascii        = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
+	unicodeChars = ranges(
+		between(384, 535), between(884, 974), between(1025, 1169), between(1328, 1423), between(1488, 1514),
+		between(1575, 1610), between(1920, 1969), between(2304, 2431), between(2433, 2554), between(2944, 3071),
+		between(3456, 3583), between(3585, 3654), between(3712, 3839), between(3840, 4031), between(4096, 4255),
+		between(4256, 4351), between(4608, 4991), between(6016, 6143), between(6144, 6319), between(7936, 8190),
+		between(12353, 12435), between(12449, 12531), between(12593, 12686), between(19968, 40869),
+	)
+)
 
+func main() {
 	out := flag.String("out", "ascii", "specify the output encoding ([ascii, hex, base32, base64])")
 	length := flag.Int("len", 50, "specify the output length")
 	set := flag.String("set", ascii, "character set to use for passwords")
@@ -42,6 +53,23 @@ func generateRandom(length int) (random []byte, err error) {
 	return
 }
 
+func between(start, end int) []int {
+	out := []int{}
+	for i := start; i <= end; i++ {
+		out = append(out, i)
+	}
+	return out
+}
+
+func ranges(rngs ...[]int) []int {
+	out := []int{}
+	for _, rng := range rngs {
+		out = append(out, rng...)
+	}
+
+	return out
+}
+
 func prepare(set string, out string, random []byte) (output string) {
 	switch out {
 	case "hex":
@@ -52,11 +80,27 @@ func prepare(set string, out string, random []byte) (output string) {
 		return base64.StdEncoding.EncodeToString(random)
 	case "ascii":
 		return getASCII(set, random)
+	case "unicode":
+		return getUnicode(len(random))
 	default:
 		log.Fatalf("invalid output type %q", out)
 	}
 
 	return ""
+}
+
+func getUnicode(length int) string {
+	runes := []rune{}
+	for i := 0; i < length; i++ {
+		i, err := rand.Int(rand.Reader, big.NewInt(math.MaxInt64))
+		if err != nil {
+			log.Fatalf("error generating random unicode character: %v", err)
+		}
+
+		runes = append(runes, rune(unicodeChars[int(i.Int64())%len(unicodeChars)]))
+	}
+
+	return string(runes)
 }
 
 func getASCII(set string, random []byte) string {
